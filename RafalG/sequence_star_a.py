ca100b5090f8a6ea25b3870366980806995cc589
RafalGromulski <rafalgromulski@wp.pl>
2021-12-20 18:20:57 +0100

    Rafał

---
+++ RafalG/sequence_star_a.py
@@ -0,0 +1,45 @@
+"""
+* Assignment: Sequence UnpackStar List
+* Required: yes
+* Complexity: easy
+* Lines of code: 1 lines
+* Time: 2 min
+
+English:
+    1. Separate ip address from host names
+    2. Use asterisk `*` notation
+    3. Run doctests - all must succeed
+
+Polish:
+    1. Odseparuj adres ip od nazwy hostów
+    2. Skorzystaj z notacji z gwiazdką `*`
+    3. Uruchom doctesty - wszystkie muszą się powieść
+
+Hints:
+    * `str.split()` without any argument
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+
+    >>> type(ip)
+    <class 'str'>
+    >>> type(hosts)
+    <class 'list'>
+    >>> assert all(type(host) is str for host in hosts)
+    >>> '' not in hosts
+    True
+    >>> ip
+    '10.13.37.1'
+    >>> hosts
+    ['nasa.gov', 'esa.int', 'roscosmos.ru']
+"""
+
+DATA = ['10.13.37.1', 'nasa.gov', 'esa.int', 'roscosmos.ru']
+
+# str: ip address: '10.13.37.1'
+# ip = DATA[0]
+
+# list[str]: list of host names: ['nasa.gov', 'esa.int', 'roscosmos.ru']
+# hosts = DATA[1:]
+
+ip, *hosts = DATA
