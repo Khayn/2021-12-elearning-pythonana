10528e513b0778f967a2e394ab41bb42e9c5cc41
RafalGromulski <rafalgromulski@wp.pl>
2022-01-09 16:10:18 +0100

    Rafal 09.01

---
+++ RafalG/2_python_controlflow/function_args_a.py
@@ -0,0 +1,35 @@
+"""
+* Assignment: Function Arguments Sequence
+* Required: yes
+* Complexity: easy
+* Lines of code: 2 lines
+* Time: 3 min
+
+English:
+    1. Define function which takes sequence of integers as an argument
+    2. Sum only even numbers
+    3. Run doctests - all must succeed
+
+Polish:
+    1. Zdefiniuj funkcję biorącą sekwencję liczb całkowitych jako argument
+    2. Zsumuj tylko parzyste liczby
+    3. Uruchom doctesty - wszystkie muszą się powieść
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+    >>> from inspect import isfunction
+
+    >>> isfunction(total)
+    True
+    >>> total([1,2,3,4])
+    6
+    >>> total([2,-1,0,2])
+    4
+    >>> total(range(0,101))
+    2550
+"""
+
+
+def total(data):
+    result = [number for number in data if number % 2 == 0]
+    return sum(result)
